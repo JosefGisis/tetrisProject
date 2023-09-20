9ee3ab1b72f92f647e108cb1011bc2be0f42fe0c
josef gisis <jsquared.gisis@gmail.com>
2023-09-20 11:02:28 -0400

    updated and button module

---
+++ button.py
@@ -0,0 +1,39 @@
+"""
+button class for generating interactive buttons.
+Takes blit surface target, location, and two images. The second image appears when the cursor is hovering over the
+button (add the same image to skip this feature)
+The update button function checks if the mouse is clicked for the first time and if the cursor is hovering over the
+button's rect and returns true is conditions are met. Otherwise, the default button image is displayed.
+"""
+
+import pygame, sys
+pygame.init()
+
+
+def help():
+    help_message = """\nbutton.Button() creates a button object that returns True when the cursor is hovering over the
+    \rbutton surface and the left mouse button is depressed.\n\nbutton.Button(self, surface, location, image1, image2)
+    \rsurface: surface blitting destination\rimage1: default button image
+    \rimage2: displayed when the cursor is hovering over the button (add the same to skip this feature)"""
+    print(help_message)
+
+
+class Button:
+    def __init__(self, surface, location, image1, image2):
+        self.surface = surface
+        self.image1, self.image2 = image1, image2
+        self.rect1, self.rect2 = self.image1.get_rect(), self.image2.get_rect()
+        self.left, self.top = location
+        self.right, self.bottom = self.left + self.image1.get_width(), self.top + self.image1.get_height()
+        self.clicked = False
+
+    def update_button(self):
+        self.surface.blit(self.image1, (self.left, self.top))
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        mouse_pos = pygame.mouse.get_pos()
+        if self.left <= mouse_pos[0] <= self.right and self.top <= mouse_pos[1] <= self.bottom:
+            self.surface.blit(self.image2, (self.left, self.top))
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                return True
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ structures each tetromino shape, and surface (regarding segments) refers to imag
 their appearance and give them colors.
 """
 import sys, pygame, random
+import button
 
 
 class Scoreboard():  # this is the scoreboard object that displays and tracks scores
@@ -54,51 +55,6 @@ class Segment(pygame.sprite.Sprite):
         self.image = surface
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
-
-
-"""
-This is a button class that uses pygame's draw.rect function. This class allows the user to create
-buttons with a custom size, position, font, text, and corner roundness. However, the fact that this class
-uses a draw function rather than than a rect or surface object comes with some limitations (namely that
-I cannot reference a rect object).
-
-The button text is a seperate function because it allows the flexibility to generate the text individually. This
-is especially useful when the program needs to update the button.
-
-The update button functions checks if the mouse is hovering over the button and returns True if it is. The
-function checks if the mouse is within the self.left, top, width, height attributes rather than checking the
-coordinates of the button because I used a draw function rather than a Rect object.   
-"""
-
-
-class Drawnbutton:
-    def __init__(self, surface, color, dimensions, text):
-        pygame.draw.rect(surface, color, dimensions, 0, 7)
-        self.left, self.top, self.width, self.height = dimensions[0], dimensions[1], dimensions[2], dimensions[3]
-        self.text = text
-
-    def button_text(self, color, font):
-        button_font_size = int(self.height * 0.80) #scales size of text based on button size
-        button_font = pygame.font.SysFont(font, button_font_size)
-        button_text_surf = button_font.render(self.text, True, color) #True confirms anti aliasing
-        """
-        This code finds the placement of the text by comparing the size of the text compared to the 
-        button and finds its staring positions.
-        """
-        button_text_pos = [(((self.width - button_text_surf.get_width()) // 2) + self.left),
-                           (((self.height - button_text_surf.get_height()) // 2) + self.top)]
-        screen.blit(button_text_surf, button_text_pos)
-
-    def update_button(self, color):
-        mouse_pos = pygame.mouse.get_pos()
-        if mouse_pos[0] >= self.left and mouse_pos[0] <= (self.left + self.width):
-            if mouse_pos[1] >= self.top and mouse_pos [1] <= (self.top + self.height):
-                pygame.draw.rect(screen, color, [self.left, self.top, self.width, self.height], 0, 7)
-                return True
-            else:
-                return None
-        else:
-            None
 
 
 """
@@ -638,7 +594,13 @@ scoreboard_rect = [((play_surface_left - SEGMENT_SIZE * 6) // 2), play_surface_t
                    (SEGMENT_SIZE * 16 + 10)]  # the plus 10 is just the offset determined by the banners
 scoreboard = Scoreboard(scoreboard_rect, [0, 1, 0, -10])
 gen_next()
-
+button_left = (screen_width - 500) / 2  # gets the starting point of the button
+start_button = button.Button(screen, (button_left, 315),
+                             pygame.image.load("start_button1.jpg"), pygame.image.load("start_button2.jpg"))
+help_button = button.Button(screen, (button_left, 415),
+                            pygame.image.load("start_button3.jpg"), pygame.image.load("start_button4.jpg"))
+exit_button = button.Button(screen, (button_left, 515),
+                            pygame.image.load("start_button5.jpg"), pygame.image.load("start_button6.jpg"))
 
 def start_menu():
     running = True
@@ -646,72 +608,32 @@ def start_menu():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN: #checks if the mouse is being depressed
-                """checks if any of the buttons are returning a hovering states (e.g. select start)
-                which means the player is hovering over that particular button."""
-                if select_start:
-                    state = "start"
-                    return state
-                elif select_help:
-                    state = "help and info"
-                    return state
-                elif select_exit:
-                    state = "exit"
-                    return state
-                else: None
-            else: None
+            elif event.type == pygame.KEYDOWN:
+                if pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                    if event.key == pygame.K_g:
+                        return "start"
+                    elif event.key == pygame.K_h:
+                        return "help and info"
+                    elif event.key == pygame.K_x:
+                        running = False
 
         screen.blit(bg_img, (0, 0))
-        """
-        >>> Below is a list of messages to be displayed on the start menu (these may be placed
-        in a list at some later time).     
-        >>> The positions (pos) are set in the middle of the screen by by subtracting the width
-        of the screen with the width of the message and diving that by two. Buttom messages
-        can be centered by using the height of the buttons as well.
-        """
         title_surf = banner_font.render("TITLE PLACEHOLDER", True, color_dict["white"])
         title_pos = (((screen_width - title_surf.get_width())/2), 150)
         screen.blit(title_surf, title_pos)
-        copyrite_surf = copyrite_font.render("©️ 2023 Josef Gisis", True, color_dict["white"])
+        copyrite_surf = copyrite_font.render("©️ 2023 Josef Gisis - v 1.0", True, color_dict["white"])
         copyrite_pos = (((screen_width - copyrite_surf.get_width())/2), 625)
         screen.blit(copyrite_surf, copyrite_pos)
-        """
-        >>> Below are the three buttons for the start menu
-        >>> I will probably include a button update function that checks to see if the
-        mouse position is hovering over the area of the button and will update the 
-        color of the button if it is. Because the update is called each frame, when
-        the mouse is no longer hovering over the button the buttons will be set back
-        to their darker shades.
-        >>> This update function will work alongside the event handler to see if the mouse
-        button is being depressed. When it is depressed the button will also change the 
-        game state. 
-        This is also the first method for displaying these buttons. Later updates may include
-        a loop to deal with the buttons. A loop would require tha drawnbutton class to be
-        updated as well.
-        Each button contains an initializing function, an update function, and text function.
-        Each variable and function is associated with the button's name for clarity.
-        """
-        button_left = (screen_width - 500) / 2 #gets the starting point of the button
-        button_width = 500
-        button_height = 75
-        start_button = Drawnbutton(screen, color_dict["darker purple"],
-                                   [button_left, 315, button_width, button_height],"START")
-        select_start = start_button.update_button(color_dict["dark purple"])
-        start_button.button_text(color_dict["white"], main_font)
 
-        help_button = Drawnbutton(screen, color_dict["darker purple"],
-                                  [button_left, 415, button_width, button_height], "HELP & INFO")
-        select_help = help_button.update_button(color_dict["dark purple"])
-        help_button.button_text(color_dict["white"], main_font)
-
-        exit_button = Drawnbutton(screen, color_dict["darker purple"],
-                                  [button_left, 515, button_width, button_height], "EXIT")
-        select_exit = exit_button.update_button(color_dict["dark purple"])
-        exit_button.button_text(color_dict["white"], main_font)
+        if start_button.update_button():
+            return "start"
+        elif help_button.update_button():
+            return "help and info"
+        elif exit_button.update_button():
+            running = False
 
         pygame.display.flip()
-
-    pygame.quit()
+    return "exit"
 
 """
 This is the main game loop.
@@ -751,7 +673,7 @@ def start_game():
                         game_over = True
                     elif event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_ESCAPE:
-                            game_state = "main menu"
+                            game_state = "pause menu"
                             return game_state
                         elif event.key == pygame.K_RIGHT:
                             """
@@ -826,7 +748,21 @@ def start_game():
 
 
 def pause_menu():
-    pass
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    game_state = "start"
+                    return game_state
+
+        pygame.draw.rect(screen, color_dict["black"],
+                         ((screen_width - 8 * SEGMENT_SIZE) // 2, (screen_height - 18 * SEGMENT_SIZE) // 2,
+                          8 * SEGMENT_SIZE, 18 * SEGMENT_SIZE))
+        pygame.display.flip()
+    pygame.quit()
 
 """This is a placeholder function"""
 def game_over():
