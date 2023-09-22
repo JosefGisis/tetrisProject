e867c5ca88a90876fe24a3ffe055cc50231e888f
josef gisis <jsquared.gisis@gmail.com>
2023-09-22 16:46:21 -0400

    second version of warning box

--- button.py
+++ button.py
@@ -44,56 +44,60 @@ class Button:
 
 class WarningBox:
     def __init__(self, surface, rect, message, buttons):
-        self.surface = surface
-        self.message = message
+        self.surface = surface  # surface where the warning box will be blitted
+        self.message = message  # warning box message
         self.left, self.top, self.width, self.height = rect
-        self.box = pygame.Surface((self.width, self.height))
-        self.button1 = pygame.Surface((self.width // 4, self.height // 8))
-        self.button2 = pygame.Surface((self.width // 4, self.height // 8))
-        self.button1_left, self.button2_left, = (self.width // 20) * 4, (self.width // 20) * 11
-        self.button1_right, self.button2_right = (self.button1_left + self.button1.get_width(),
-                                                  self.button2_left + self.button2.get_width())
-        self.button_top = (self.height // 8) * 5
-        self.button_bottom = self.button_top + self.button1.get_height()
-        self.title_font = pygame.font.SysFont("ocraextended", int(self.width / len(self.message) * 1.2))
-        self.button_font = pygame.font.SysFont("ocraextended", int(0.5 * self.button1.get_height()))
-        self.message_surf = self.title_font.render(self.message, True, (255, 255, 255))
-        self.button1_text_surf = self.button_font.render(buttons[0], True, (255, 255, 255))
-        self.button2_text_surf = self.button_font.render(buttons[1], True, (255, 255, 255))
-        self.message_left, self.message_top = (self.width - self.message_surf.get_width()) // 2, int(0.2 * self.height)
-        self.button1_text_left = (self.button1.get_width() - self.button1_text_surf.get_width()) // 2
-        self.button2_text_left = (self.button2.get_width() - self.button2_text_surf.get_width()) // 2
-        self.button_text_top = (self.height // 8 - self.button1_text_surf.get_height()) // 2
+        # msg font is determined by a coefficient (1.2), the width of the box, and the size of the message
+        self.msg_font = pygame.font.SysFont("ocraextended", int(self.width / len(self.message) * 1.2))
 
-    def display_box(self):
-        self.button1.fill((50, 0, 75))
-        self.button2.fill((50, 0, 75))
-        self.button1.blit(self.button1_text_surf, (self.button1_text_left, self.button_text_top))
-        self.button2.blit(self.button2_text_surf, (self.button2_text_left, self.button_text_top))
-        self.box.blit(self.message_surf, (self.message_left, self.message_top))
-        self.box.blit(self.button1, (self.button1_left, self.button_top))
-        self.box.blit(self.button2, (self.button2_left, self.button_top))
-        self.surface.blit(self.box, (self.left, self.top))
+        # warning box surface object
+        self.box = pygame.Surface((self.width, self.height))
+
+        # buttons surfaces and properties. button sizes are 1/4 the width and 1/8 the height of the box
+        self.btn1 = pygame.Surface((self.width // 4, self.height // 8))
+        self.btn2 = pygame.Surface((self.width // 4, self.height // 8))
+        # buttons are positioned by breaking up the size of the warning box into 20 segments
+        self.button1_left, self.button2_left, = (self.width // 20) * 4, (self.width // 20) * 11
+        self.btn1_right = self.button1_left + self.btn1.get_width()
+        self.btn2_right = self.button2_left + self.btn2.get_width()
+        self.btn_top = (self.height // 8) * 5
+        self.btn_bottom = self.btn_top + self.btn1.get_height()
+
+        # message surface and properties
+        self.msg_surf = self.msg_font.render(self.message, True, (255, 255, 255))
+        self.msg_left, self.msg_top = (self.width - self.msg_surf.get_width()) // 2, int(0.2 * self.height)
+
+        # button text surfaces and properties
+        self.button_font = pygame.font.SysFont("ocraextended", int(0.5 * self.btn1.get_height()))
+        self.text1_surf = self.button_font.render(buttons[0], True, (255, 255, 255))
+        self.text1_left = (self.btn1.get_width() - self.text1_surf.get_width()) // 2
+        self.text2_surf = self.button_font.render(buttons[1], True, (255, 255, 255))
+        self.text2_left = (self.btn2.get_width() - self.text2_surf.get_width()) // 2
+        self.text_top = (self.height // 8 - self.text1_surf.get_height()) // 2
 
     def update_box(self):
         mouse_posx, mouse_posy = pygame.mouse.get_pos()
-        mouse_pos = mouse_posx - self.left, mouse_posy - self.top
-        if self.button1_left <= mouse_pos[0] <= self.button1_right\
-                and self.button_top <= mouse_pos[1] <= self.button_bottom:
-            self.button1.fill((75, 25, 100))
-            self.button1.blit(self.button1_text_surf, (self.button1_text_left, self.button_text_top))
-            self.box.blit(self.button1, (self.button1_left, self.button_top))
-            self.surface.blit(self.box, (self.left, self.top))
+        mouse_pos = mouse_posx - self.left, mouse_posy - self.top  # gets the surfaces relative position
+        if self.button1_left <= mouse_pos[0] <= self.btn1_right and self.btn_top <= mouse_pos[1] <= self.btn_bottom:
+            self.btn1.fill((75, 25, 100))
             if pygame.mouse.get_pressed()[0] == 1:
                 return True
-        if self.button2_left <= mouse_pos[0] <= self.button2_right\
-                and self.button_top <= mouse_pos[1] <= self.button_bottom:
-            self.button2.fill((75, 25, 100))
-            self.button2.blit(self.button2_text_surf, (self.button2_text_left, self.button_text_top))
-            self.box.blit(self.button2, (self.button2_left, self.button_top))
-            self.surface.blit(self.box, (self.left, self.top))
+        else:
+            self.btn1.fill((50, 0, 75))
+
+        if self.button2_left <= mouse_pos[0] <= self.btn2_right and self.btn_top <= mouse_pos[1] <= self.btn_bottom:
+            self.btn2.fill((75, 25, 100))
             if pygame.mouse.get_pressed()[0] == 1:
                 return False
+        else:
+            self.btn2.fill((50, 0, 75))
+
+        self.btn1.blit(self.text1_surf, (self.text1_left, self.text_top))
+        self.btn2.blit(self.text2_surf, (self.text2_left, self.text_top))
+        self.box.blit(self.msg_surf, (self.msg_left, self.msg_top))
+        self.box.blit(self.btn1, (self.button1_left, self.btn_top))
+        self.box.blit(self.btn2, (self.button2_left, self.btn_top))
+        self.surface.blit(self.box, (self.left, self.top))
 
 
 exit_warning = WarningBox(screen, (0, 0, 500, 300), "RESTART GAME?", ("OK", "CANCEL"))
@@ -105,7 +109,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill((255, 255, 255))
-    exit_warning.display_box()
     confirmed = exit_warning.update_box()
     if confirmed:
         running = False
