cdfbab9c3ef9b067fa5a18a9c8ebb1b3f7724f2f
josef gisis <jsquared.gisis@gmail.com>
2023-09-22 13:51:56 -0400

    first version of warning box

--- button.py
+++ button.py
@@ -48,21 +48,24 @@ class WarningBox:
         self.message = message
         self.left, self.top, self.width, self.height = rect
         self.box = pygame.Surface((self.width, self.height))
-        self.button1 = pygame.Surface((self.width // 6, self.height // 8))
-        self.button2 = pygame.Surface((self.width // 6, self.height // 8))
-        self.button1_left, self.button2_left, = (self.width // 12) * 3, (self.width // 12) * 6
+        self.button1 = pygame.Surface((self.width // 4, self.height // 8))
+        self.button2 = pygame.Surface((self.width // 4, self.height // 8))
+        self.button1_left, self.button2_left, = (self.width // 20) * 4, (self.width // 20) * 11
+        self.button1_right, self.button2_right = (self.button1_left + self.button1.get_width(),
+                                                  self.button2_left + self.button2.get_width())
         self.button_top = (self.height // 8) * 5
-        self.title_font = pygame.font.SysFont("ocraextended", int(self.width / len(self.message) * 1.5))
-        self.button_font = pygame.font.SysFont("ocraextended", int(0.6 * self.button1.get_height()))
+        self.button_bottom = self.button_top + self.button1.get_height()
+        self.title_font = pygame.font.SysFont("ocraextended", int(self.width / len(self.message) * 1.2))
+        self.button_font = pygame.font.SysFont("ocraextended", int(0.5 * self.button1.get_height()))
         self.message_surf = self.title_font.render(self.message, True, (255, 255, 255))
         self.button1_text_surf = self.button_font.render(buttons[0], True, (255, 255, 255))
         self.button2_text_surf = self.button_font.render(buttons[1], True, (255, 255, 255))
         self.message_left, self.message_top = (self.width - self.message_surf.get_width()) // 2, int(0.2 * self.height)
-        self.button1_text_left = (self.width // 6 - self.button1_text_surf.get_width()) // 2
-        self.button2_text_left = (self.width // 6 - self.button2_text_surf.get_width()) // 2
+        self.button1_text_left = (self.button1.get_width() - self.button1_text_surf.get_width()) // 2
+        self.button2_text_left = (self.button2.get_width() - self.button2_text_surf.get_width()) // 2
         self.button_text_top = (self.height // 8 - self.button1_text_surf.get_height()) // 2
 
-    def update_box(self):
+    def display_box(self):
         self.button1.fill((50, 0, 75))
         self.button2.fill((50, 0, 75))
         self.button1.blit(self.button1_text_surf, (self.button1_text_left, self.button_text_top))
@@ -72,8 +75,28 @@ class WarningBox:
         self.box.blit(self.button2, (self.button2_left, self.button_top))
         self.surface.blit(self.box, (self.left, self.top))
 
+    def update_box(self):
+        mouse_posx, mouse_posy = pygame.mouse.get_pos()
+        mouse_pos = mouse_posx - self.left, mouse_posy - self.top
+        if self.button1_left <= mouse_pos[0] <= self.button1_right\
+                and self.button_top <= mouse_pos[1] <= self.button_bottom:
+            self.button1.fill((75, 25, 100))
+            self.button1.blit(self.button1_text_surf, (self.button1_text_left, self.button_text_top))
+            self.box.blit(self.button1, (self.button1_left, self.button_top))
+            self.surface.blit(self.box, (self.left, self.top))
+            if pygame.mouse.get_pressed()[0] == 1:
+                return True
+        if self.button2_left <= mouse_pos[0] <= self.button2_right\
+                and self.button_top <= mouse_pos[1] <= self.button_bottom:
+            self.button2.fill((75, 25, 100))
+            self.button2.blit(self.button2_text_surf, (self.button2_text_left, self.button_text_top))
+            self.box.blit(self.button2, (self.button2_left, self.button_top))
+            self.surface.blit(self.box, (self.left, self.top))
+            if pygame.mouse.get_pressed()[0] == 1:
+                return False
 
-exit_warning = WarningBox(screen, (0, 0, 800, 500), "RESTART GAME?", ("OK", "CANCEL"))
+
+exit_warning = WarningBox(screen, (0, 0, 500, 300), "RESTART GAME?", ("OK", "CANCEL"))
 
 
 running = True
@@ -82,7 +105,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill((255, 255, 255))
-    exit_warning.update_box()
+    exit_warning.display_box()
+    confirmed = exit_warning.update_box()
+    if confirmed:
+        running = False
     pygame.display.flip()
 
 pygame.quit()
