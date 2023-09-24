384b46060faa00197e4f4d4d9f0031361a5eee36
josef gisis <jsquared.gisis@gmail.com>
2023-09-24 15:48:20 -0400

    improved warning box and text buttons

--- button.py
+++ button.py
@@ -12,10 +12,7 @@ screen = pygame.display.set_mode((1000, 800))
 
 
 def help():
-    help_message = """\nbutton.Button() creates a button object that returns True when the cursor is hovering over the
-    \rbutton surface and the left mouse button is depressed.\n\nbutton.Button(self, surface, location, image1, image2)
-    \rsurface: surface blitting destination\rimage1: default button image
-    \rimage2: displayed when the cursor is hovering over the button (add the same to skip this feature)"""
+    help_message = """l"""
     print(help_message)
 
 
@@ -62,17 +59,19 @@ class TextButton:
         mouse_posx, mouse_posy = pygame.mouse.get_pos()
         mouse_pos = mouse_posx - self.relative_pos[0], mouse_posy - self.relative_pos[1]
         if self.left <= mouse_pos[0] <= self.right and self.top <= mouse_pos[1] <= self.bottom:
-            self.button_surf.fill
+            self.button_surf.fill((75, 25, 100))
             if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
                 self.clicked = True
                 return True
-
+        else:
+            self.button_surf.fill((50, 0, 75))
+        self.button_surf.blit(self.text_surf, (self.text_left, self.text_top))
         self.surface.blit(self.button_surf, (self.left, self.top))
 
+
 class WarningBox:
-    def __init__(self, surface, rect, message, buttons):
+    def __init__(self, surface, rect, message):
         self.surface = surface  # surface where the warning box will be blitted
-        self.message = message  # warning box message
         self.left, self.top, self.width, self.height = rect
 
         # warning box surface object
@@ -80,16 +79,34 @@ class WarningBox:
 
         # message surface and properties
         # msg font is determined by a coefficient (1.2), the width of the box, and the size of the message
-        self.msg_font = pygame.font.SysFont("ocraextended", int(self.width / len(self.message) * 1.2))
+        self.msg_font = pygame.font.SysFont("ocraextended", int(self.width / len(message) * 1.2))
+        self.msg_surf = self.msg_font.render(message, True, (255, 255, 255))
         self.msg_left, self.msg_top = (self.width - self.msg_surf.get_width()) // 2, int(0.2 * self.height)
 
-    def update_box(self):
+    # displays warning box
+    def display_box(self):
         self.box.blit(self.msg_surf, (self.msg_left, self.msg_top))
         self.surface.blit(self.box, (self.left, self.top))
 
+    # returns rect for one of two optional buttons
+    def button1_rect(self):
+        button_width, button_height = self.width // 4, self.height // 8
+        button1_left = (self.width // 20) * 4
+        button_top = (self.height // 8) * 5
+        return button1_left, button_top, button_width, button_height
 
-exit_warning = WarningBox(screen, (0, 0, 500, 300), "RESTART GAME?", ("OK", "CANCEL"))
+    # returns rect of of two of two optional buttons
+    def button2_rect(self):
+        button_width, button_height = self.width // 4, self.height // 8
+        button2_left = (self.width // 20) * 11
+        button_top = (self.height // 8) * 5
+        return button2_left, button_top, button_width, button_height
 
+
+exit_warning = WarningBox(screen, (200, 300, 500, 300), "WOULD YOU LIKE?")
+# using exit warning box to determine location and dimensions of buttons
+ok_btn = TextButton(exit_warning.box, exit_warning.button1_rect(), "OK", (exit_warning.left, exit_warning.top))
+cancel_btn = TextButton(exit_warning.box, exit_warning.button2_rect(), "CANCEL", (exit_warning.left, exit_warning.top))
 
 running = True
 while running:
@@ -97,9 +114,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill((255, 255, 255))
-    confirmed = exit_warning.update_box()
-    if confirmed:
+    exit_warning.display_box()
+    if ok_btn.update_button():
         running = False
+    cancel_btn.update_button()
     pygame.display.flip()
 
 pygame.quit()
